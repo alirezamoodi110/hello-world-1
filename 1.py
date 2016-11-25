2b48bcf6347a249fba3cdbb3caedfaf6855888c5
Amir Asadi <a.asadi94@gmail.com>
2016-11-25 22:14:13 +0330

    I've just added two functions for more functionallity they are very goor for better read and write ability theire name is "inputoutput" and "calculation"

---
+++ 1.py
@@ -0,0 +1,16 @@
+max = min = 0
+def inputoutput():
+    max = input('Enter a number : ')
+    min = input('Enter another number : ')
+
+def selection():
+    if max>min:
+        print('max is {0} , min is {1} '.format(max,min))
+    else:
+        max,min=min,max
+        print('max is {0} , min is {1} '.format(max, min))
+
+inputoutput()
+selection()
+
+
